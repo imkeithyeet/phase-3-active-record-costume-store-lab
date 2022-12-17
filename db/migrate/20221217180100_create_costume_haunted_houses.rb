9606883e7d0b1bb8690f30205ccbe4528f5f5480
Keith Ma <keithchezzy@gmail.com>
2022-12-17 10:14:25 -0800

    commit tree 40e297ef9fc6aac0d89840447b03a946b755d67c

---
+++ db/migrate/20221217180100_create_costume_haunted_houses.rb
@@ -0,0 +1,14 @@
+class CreateCostumeHauntedHouses < ActiveRecord::Migration[6.1]
+  def change
+    def change
+      create_table :haunted_houses do |t|
+        t.string :name
+        t.string :location
+        t.integer :costume_inventory 
+        t.integer :num_of_employees
+        t.boolean :is_in_business 
+        t.datetime :opening_time
+        t.datetime :closing_time
+      end
+  end
+end
